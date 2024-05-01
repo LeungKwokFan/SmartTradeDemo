37ee1d051ec8ffdd04b77748b9a0b896de660c23
LeungKwokFan <klkouson1204@gmail.com>
2024-05-01 15:01:44 +0800

    Login page set.

--- SmartTradeDemo/SmartTradeDemo/ViewController.swift
+++ SmartTradeDemo/SmartTradeDemo/ViewController.swift
@@ -6,11 +6,19 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var signUpButton: UIButton!
+    
     @IBOutlet weak var loginButton: UIButton!
+    
+    var videoPlayer:AVPlayer?
+    
+    var videoPlayerLayer:AVPlayerLayer?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -22,9 +30,37 @@ class ViewController: UIViewController {
         Utilities.styleFilledButton(signUpButton)
         
         Utilities.styleFilledButton(loginButton)
+        
+    }
     
+    override func viewWillAppear(_ animated: Bool) {
+        let bundlePath = Bundle.main.path(forResource: "joey", ofType: "mp4")
+        
+        guard bundlePath != nil else{return}
+        
+        let url = URL(filePath: bundlePath!)
+        
+        let item = AVPlayerItem(url: url)
+        
+        videoPlayer = AVPlayer(playerItem: item)
+        
+        videoPlayerLayer = AVPlayerLayer(player: videoPlayer!)
+        
+//        videoPlayer?.isMuted = true
         
         
+        videoPlayerLayer?.frame = CGRect(x: -self.view.frame.size.width*1.5, y: 0, width: self.view.frame.size.width*4, height: self.view.frame.size.height)
+        
+        
+        view.layer.insertSublayer(videoPlayerLayer!, at: 0)
+        
+        videoPlayer?.playImmediately(atRate: 1)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        videoPlayer?.pause()
     }
 
 }
